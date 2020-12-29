25700b25c324d1fa27802874e1378246ea0b1f4e
kris4oo <iceninja569@gmail.com>
2020-12-29 21:57:41 +0200

    Added first functions

---
+++ Chekii/Functions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chekii
+{
+    public class Functions
+    {
+        public static void Threader(ThreadStart module)
+        {
+            for(int i =0; i<Vars.threads; i++)
+            {
+                new Thread(module).Start();
+            }
+        }
+
+        public static string Parse(string src,string left,string right)
+        {
+            return src.Split(new string[]
+            {
+                left
+            }, StringSplitOptions.None)[1].Split(new string[]
+            {
+                right
+            }, StringSplitOptions.None)[0];
+        }
+        public static string ParseLeft(string src, string left)
+        {
+            return src.Split(new string[]
+            {
+                left
+            }, StringSplitOptions.None)[1];
+        }
+
+        
+
+        
+    }
+}
--- Chekii/Vars.cs
+++ Chekii/Vars.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace Chekii
 {
-    public class Class1
+    public static class Vars
     {
+        public static int threads;
     }
 }
